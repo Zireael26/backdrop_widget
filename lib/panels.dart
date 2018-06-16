e9f2c1af02ab75ceacefa401c4373b00043ed5ed
Abhishek Kaushik <abhishek.nexus26@gmail.com>
2018-06-17 01:03:53 +0530

    Added slide up/down animation, finishing touches

--- lib/panels.dart
+++ lib/panels.dart
@@ -12,7 +12,20 @@ class Panels extends StatefulWidget {
 
 class _PanelsState extends State<Panels> {
   // Height for our header
-  static const headerHeight = 96.0;
+  static const headerHeight = 32.0;
+
+  Animation<RelativeRect> getPanelAnimation(BoxConstraints constraints) {
+    final height = constraints.biggest.height;
+    final backPanelHeight = height - headerHeight;
+    final frontPanelHeight = -headerHeight;
+
+    return RelativeRectTween(
+      begin: RelativeRect.fromLTRB(0.0, backPanelHeight, 0.0, frontPanelHeight),
+      end: RelativeRect.fromLTRB(0.0, 0.0, 0.0, 0.0),
+    ).animate(
+      CurvedAnimation(parent: widget.controller, curve: Curves.linear),
+    );
+  }
 
   // Front and Back Panels widget
   Widget fnbPanels(BuildContext context, BoxConstraints constraints) {
@@ -26,76 +39,115 @@ class _PanelsState extends State<Panels> {
           //Back Panel
           Container(
             color: theme.primaryColor,
-            child: Column(
-              children: <Widget>[
-                Text(
-                  'Header 1',
-                  style: TextStyle(fontSize: 16.0, color: Colors.indigoAccent),
-                ),
-                Text(
-                  'Item 1',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    color: Colors.white,
-                  ),
-                ),
-                Text(
-                  'Item 2',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    color: Colors.white,
-                  ),
-                ),
-                Text(
-                  'Header 2',
-                  style: TextStyle(fontSize: 16.0, color: Colors.indigoAccent),
-                ),
-                Text(
-                  'Item 1',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    color: Colors.white,
-                  ),
-                ),
-                Text(
-                  'Item 2',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ],
-            ),
-          ),
-
-          // Front Panel
-          Material(
-              elevation: 16.0,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(16.0),
-                topRight: Radius.circular(16.0),
-              ),
+            child: new Padding(
+              padding: const EdgeInsets.all(16.0),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: <Widget>[
-                  Container(
-                    height: headerHeight,
-                    child: new Center(
-                        child: Text(
-                      'Front Panel',
-                      style:
-                          TextStyle(fontSize: 32.0, color: theme.accentColor),
-                    )),
-                  ),
-                  Expanded(
-                    child: new Center(
-                      child: Text(
-                        'Dummy Contents',
-                        style: TextStyle(fontSize: 24.0),
+                  new Padding(
+                    padding: const EdgeInsets.only(top: 16.0),
+                    child: Text(
+                      'Header 1',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.indigoAccent,
                       ),
                     ),
-                  )
+                  ),
+                  new Padding(
+                    padding: const EdgeInsets.only(top: 8.0),
+                    child: Text(
+                      'Item 1',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  new Padding(
+                    padding: const EdgeInsets.only(top: 8.0),
+                    child: Text(
+                      'Item 2',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  new Padding(
+                    padding: const EdgeInsets.only(top: 16.0),
+                    child: Divider(
+                      height: 2.0,
+                    ),
+                  ),
+                  new Padding(
+                    padding: const EdgeInsets.only(top: 16.0),
+                    child: Text(
+                      'Header 2',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.indigoAccent,
+                      ),
+                    ),
+                  ),
+                  new Padding(
+                    padding: const EdgeInsets.only(top: 8.0),
+                    child: Text(
+                      'Item 1',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  new Padding(
+                    padding: const EdgeInsets.only(top: 8.0),
+                    child: Text(
+                      'Item 2',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
                 ],
-              ))
+              ),
+            ),
+          ),
+          // Front Panel, a child of [PositionedTransition], which takes the anim we made
+          new PositionedTransition(
+            rect: getPanelAnimation(constraints),
+            child: Material(
+                elevation: 16.0,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(16.0),
+                  topRight: Radius.circular(16.0),
+                ),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      height: headerHeight,
+                      child: new Center(
+                          child: Text(
+                        'Front Panel',
+                        style: TextStyle(
+                          fontSize: 32.0,
+                          color: theme.accentColor,
+                        ),
+                      )),
+                    ),
+                    Expanded(
+                      child: new Center(
+                        child: Text(
+                          'Dummy Contents',
+                          style: TextStyle(fontSize: 24.0),
+                        ),
+                      ),
+                    )
+                  ],
+                )),
+          )
         ],
       ),
     );
