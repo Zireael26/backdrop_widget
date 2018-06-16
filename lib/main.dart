3a3838b5b6618a8517beefa70173249b40edad12
Abhishek Kaushik <abhishek.nexus26@gmail.com>
2018-06-16 23:38:23 +0530

    Added hamburger menu button on AppBar Added animation for the button

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Backdrop Demo',
       theme: new ThemeData(
         primarySwatch: Colors.indigo,
@@ -23,12 +24,53 @@ class BackDropHomePage extends StatefulWidget {
   _BackDropHomePageState createState() => _BackDropHomePageState();
 }
 
-class _BackDropHomePageState extends State<BackDropHomePage> {
+class _BackDropHomePageState extends State<BackDropHomePage>
+    with SingleTickerProviderStateMixin {
+  // Instantiate an [AnimationController] for the hamburger menu icon
+  AnimationController animController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Initialize the [AnimationController] to animate in 500ms
+    animController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 500),
+      value: 1.0,
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animController.dispose();
+  }
+
+  // To check animation status
+  bool get isPanelVisible {
+    AnimationStatus animStatus = animController.status;
+    return animStatus == AnimationStatus.completed ||
+        animStatus == AnimationStatus.forward;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: new AppBar(
         title: new Text('BackDrop'),
+        leading: new IconButton(
+          icon: AnimatedIcon(
+            icon: AnimatedIcons.close_menu,
+            progress: animController.view,
+          ),
+          onPressed: () {
+            // set fling velocity based on the panel's visibility
+            animController.fling(velocity: isPanelVisible ? -1.0 : 1.0);
+          },
+        ),
+        elevation: 0.0,
+        // elevation is set to 0 because it is supposed to be part of the back panel
       ),
       body: Container(),
     );
